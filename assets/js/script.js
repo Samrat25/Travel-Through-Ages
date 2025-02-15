3c0ac2a45be401be4890d8cb51e5412e6216cdc4
Samrat25 <samratnatta993@gmail.com>
2025-02-16 05:05:16 +0530

    update in inquire form

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,3 +49,15 @@ window.addEventListener("scroll", function () {
   }
 
 });
+
+
+function handleFormSubmit(event) {
+  event.preventDefault();  // Prevent default form submission behavior
+
+  // You can add your form processing logic here (e.g., sending data to a server)
+
+  alert("Thank you for your message! We will get back to you soon.");
+
+  // Redirect back to the homepage (index page)
+  window.location.href = 'index.html';  // Make sure 'index.html' is the correct path to your homepage
+}
