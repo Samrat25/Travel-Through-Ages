693cbb0b2fc5aa5e72c234efd1bf184734190750
Samrat25 <samratnatta993@gmail.com>
2025-02-16 04:21:38 +0530

    update

--- assets/js/packages.js
+++ assets/js/packages.js
@@ -2,3 +2,11 @@ window.addEventListener("load", () => {
     const loader = document.querySelector(".loaderr");
     loader.style.transform = "translateY(-100%)";
   });
+  window.onscroll = function () {
+    if (scrollY > 50) {
+      navbar.classList.add("active");
+    } else {
+      navbar.classList.remove("active");
+    }
+  };
+  
